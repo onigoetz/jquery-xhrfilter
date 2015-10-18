5e7085ec6cce81207b316966bee2939d24fe631c
Stéphane Goetz <onigoetz@onigoetz.ch>
2015-10-18 23:55:30 +0200

    First working middleware !

--- server.php
+++ server.php
@@ -1,4 +1,9 @@
 <?php
 
 header('Content-Type: application/json');
+
+if (array_key_exists('fail', $_GET)) {
+    header('HTTP/1.0 403 Forbidden');
+}
+
 echo json_encode(['response' => 'yeah !']);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./lib/XHRMiddleware.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "XHRMiddleware.min.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel?optional[]=runtime'
+        }]
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"',
+            __DEV__: false
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {warnings: true}
+        }),
+        new webpack.optimize.AggressiveMergingPlugin()
+    ]
+};
